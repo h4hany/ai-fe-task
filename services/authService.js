d2abbe9c14f1dee3b742c6d2105dce167696cfcd
hany <hanysayed.a@gmail.com>
2025-02-28 09:46:36 +0200

    test deploy

--- services/authService.js
+++ services/authService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000";
+// const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 import qs from "qs";
 
 export const login = async (username,
